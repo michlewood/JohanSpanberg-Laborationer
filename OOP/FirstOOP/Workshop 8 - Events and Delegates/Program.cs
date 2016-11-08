934f2a359727101795c46ee6b9ba5e0aa10ee58a
Johan Spånberg <j.spanberg@live.com>
2016-11-08 09:16:33 +0100

    Initial commit.

---
+++ OOP/FirstOOP/Workshop 8 - Events and Delegates/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop_8___Events_and_Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var runtime = new Runtime();
+            runtime.Start();
+        }
+    }
+}
---
+++ OOP/FirstOOP/Workshop 8 - Events and Delegates/Runtime.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop_8___Events_and_Delegates
+{
+    class Runtime
+    {
+        public delegate void MyDelegate(string message); // delegates är som en variabel som innehåller en metod.
+                // Delegaten och Delegatmetoden måste ha samma typ av returvärde.
+
+        public void DelegateMethod(string message) // Definerar metoden som delegaten MyDelegate ska innehålla
+        {
+            Console.WriteLine(message);
+        }
+
+        public void Start()
+        {
+            MyDelegate del1 = new MyDelegate(DelegateMethod);
+
+            del1("Hello world!");
+        }
+    }
+}
